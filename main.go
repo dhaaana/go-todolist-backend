37f74ce60fb3de2e4cf27dbca5957d19df06e77b
Ramadhana R <ddhanarr@gmail.com>
2023-01-30 23:26:53 +0700

    feat: initialize mongodb and gin

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dhaaana/todolist-backend/config"
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server      *gin.Engine
+	ctx         context.Context
+	mongoclient *mongo.Client
+)
+
+func init() {
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+
+	ctx = context.TODO()
+
+	// Connect to MongoDB
+	mongoconn := options.Client().ApplyURI(config.DBUri)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := mongoclient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("MongoDB successfully connected...")
+	server = gin.Default()
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	config, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Could not load config", err)
+	}
+
+	defer mongoclient.Disconnect(ctx)
+
+	startGinServer(config)
+}
+
+func startGinServer(config config.Config) {
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "server is running"})
+	})
+
+	log.Fatal(server.Run(":" + config.Port))
 }
